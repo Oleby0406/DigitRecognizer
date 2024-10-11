b8fc495f5063b0c8a71e5a5b390219f566f54384
Oleby0406 <oleg.bychenkov@gmail.com>
2024-10-10 21:16:46 -0700

    Trying to improve accuracy a year later for fun

--- createModel.py
+++ createModel.py
@@ -2,18 +2,21 @@ from keras.datasets import mnist
 from keras.utils import to_categorical
 from keras.models import Sequential
 from keras.layers import Conv2D
+from keras.layers import Dropout
 from keras.layers import MaxPooling2D
 from keras.layers import Dense
 from keras.layers import Flatten
-from keras.optimizers import SGD
+from keras.optimizers import Adam
+from keras.optimizers.schedules import ExponentialDecay
+from keras import callbacks
 
 def load_dataset():
-	(trainX, trainY), (testX, testY) = mnist.load_data()
+	(trainX, trainY), (valX, valY) = mnist.load_data()
 	trainX = trainX.reshape((trainX.shape[0], 28, 28, 1))
-	testX = testX.reshape((testX.shape[0], 28, 28, 1))
+	valX = valX.reshape((valX.shape[0], 28, 28, 1))
 	trainY = to_categorical(trainY)
-	testY = to_categorical(testY)
-	return trainX, trainY, testX, testY
+	valY = to_categorical(valY)
+	return trainX, trainY, valX, valY
 
 def prep_pixels(train, test):
 	train_norm = train.astype('float32')
@@ -23,24 +26,42 @@ def prep_pixels(train, test):
 	return train_norm, test_norm
 
 def define_model():
+	lr_schedule = ExponentialDecay(
+    initial_learning_rate=0.0001,
+    decay_steps=10000,
+    decay_rate=0.96,
+    staircase=True)
+
 	model = Sequential()
 	model.add(Conv2D(32, (3, 3), activation='relu', kernel_initializer='he_uniform', input_shape=(28, 28, 1)))
 	model.add(MaxPooling2D((2, 2)))
+
 	model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform'))
 	model.add(Conv2D(64, (3, 3), activation='relu', kernel_initializer='he_uniform'))
 	model.add(MaxPooling2D((2, 2)))
+
 	model.add(Flatten())
-	model.add(Dense(100, activation='relu', kernel_initializer='he_uniform'))
+	model.add(Dense(128, activation='relu', kernel_initializer='he_uniform'))
+	model.add(Dropout(0.5))
 	model.add(Dense(10, activation='softmax'))
-	opt = SGD(learning_rate=0.01, momentum=0.9)
+
+	opt = Adam(learning_rate = lr_schedule)
 	model.compile(optimizer=opt, loss='categorical_crossentropy', metrics=['accuracy'])
+
 	return model
 
 def run_test_harness():
-	trainX, trainY, testX, testY = load_dataset()
-	trainX, testX = prep_pixels(trainX, testX)
+	trainX, trainY, valX, valY = load_dataset()
+	trainX, valX = prep_pixels(trainX, valX)
 	model = define_model()
-	model.fit(trainX, trainY, epochs=10, batch_size=32, verbose=1)
+
+	earlystopping = callbacks.EarlyStopping(monitor = "val_loss",
+                                        patience = 3,
+                                        mode = 'min',
+                                        restore_best_weights = True,
+                                        start_from_epoch = 3)
+	
+	model.fit(trainX, trainY, epochs=20, validation_data = [valX, valY], callbacks = [earlystopping], verbose=1)
 	model.save('final_model')
 
 run_test_harness()
